760e2601a7866750c5df22316e4f58c8de3b211b
Piotr Plewa <pplewa@atlassian.com>
2015-10-17 21:27:10 +1100

    export map

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 var Express = require('express');
 var phantom = require('phantom');
+var fs = require('fs');
+var Q = require('q');
 
 var app = Express();
 app.use(Express.static('./'));
-var server = app.listen(process.env.PORT || 3000, function() {
-	console.log('Server is listening at %s', server.address().port);
 
-	phantom.create(function (ph) {
-		ph.createPage(function (page) {
-			page.viewportSize = { width: 1024, height: 768 };
-			page.open('http://localhost:3000/static/index.html', function (status) {
-				console.log('saving screenshot');
-				page.render('map.png');
-				process.exit(0);
+function convertToMap(segments) {
+	var deferred = Q.defer();
+
+	fs.writeFileSync('static/segments.json', JSON.stringify(segments));
+	var server = app.listen(process.env.PORT || 3000, function() {
+		console.log('Server is listening at %s', server.address().port);
+
+		phantom.create(function (ph) {
+			ph.createPage(function (page) {
+				page.viewportSize = { width: 1024, height: 768 };
+				page.open('http://localhost:3000/static/index.html', function (status) {
+					console.log('saving screenshot');
+					page.render('static/map.png');
+					deferred.resolve('http://localhost:3000/static/map.png');
+					// process.exit(0);
+				});
 			});
 		});
 	});
-});
+
+	return deferred.promise;
+}
+
+module.exports.convertToMap = convertToMap;
