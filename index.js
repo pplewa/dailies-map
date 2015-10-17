6ef51a99fa6dc762dbd6dcd08c5163da3852cb4b
Piotr Plewa <pplewa@atlassian.com>
2015-10-17 21:45:21 +1100

    export map

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-var Express = require('express');
 var phantom = require('phantom');
 var fs = require('fs');
 var Q = require('q');
 
-var app = Express();
-app.use(Express.static('./'));
-
 function convertToMap(segments) {
 	var deferred = Q.defer();
 
-	fs.writeFileSync('static/segments.json', JSON.stringify(segments));
-	var server = app.listen(process.env.PORT || 3000, function() {
-		console.log('Server is listening at %s', server.address().port);
-
-		phantom.create(function (ph) {
-			ph.createPage(function (page) {
-				page.viewportSize = { width: 1024, height: 768 };
-				page.open('http://localhost:3000/static/index.html', function (status) {
-					console.log('saving screenshot');
-					page.render('static/map.png');
-					deferred.resolve('http://localhost:3000/static/map.png');
-					// process.exit(0);
-				});
+	fs.writeFileSync(__dirname + '/static/segments.js', 'window.segments = ' + JSON.stringify(segments));
+	phantom.create(function(ph) {
+		ph.createPage(function(page) {
+			page.viewportSize = { width: 1024, height: 768 };
+			page.open(__dirname + '/static/index.html', function(status) {
+				console.log('saving screenshot');
+				page.render(__dirname + '/static/map.png');
+				deferred.resolve(__dirname + '/static/map.png');
+				ph.exit();
 			});
 		});
 	});
