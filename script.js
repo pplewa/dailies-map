7972bac823d4d8b5a83dff01f0732bf217d3e867
Piotr Plewa <pplewa@atlassian.com>
2015-10-20 20:05:48 +1100

    screenshot fixes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var page = require('webpage').create();
 var system = require('system');
 page.open(system.args[1] + '/static/index.html', function(status) {
 	console.log('saving screenshot');
-	page.render(system.args[1] + '/static/map.png');
-	phantom.exit();
+	setTimeout(function(){
+		page.render(system.args[1] + '/static/map.png');
+		phantom.exit();
+	}, 1000);
 });
